a485ca956b4d010a0420ce89db9e18e27a3f4a4f
Виктор <v.yu.linin@4their.ru>
2019-05-29 00:35:33 +0300

    отладка телеграмм

--- app/Http/Controllers/telegramController.php
+++ app/Http/Controllers/telegramController.php
@@ -17,23 +17,8 @@ class telegramController extends Controller
 
         $set_webhook='https://api.telegram.org/bot845701278:AAG-eaVtv4oNOjhYOSHGaNU6DPvb-ml3P2k/setwebhook?url=https://xn--h1aahjb.xn--p1acf/tlgrm';
         $data = json_decode($request->getContent());
-//        https://api.telegram.org/bot845701278:AAG-eaVtv4oNOjhYOSHGaNU6DPvb-ml3P2k/getUpdates
-//        $proxy='64.118.88.39:19485';
-//        $response = array(
-//            'chat_id' =>  331906939,
-//            'text' =>$data->message->chat->id. ' '.$data->message->text,
-////            'reply_markup' => $this->keyboard
-//        );
-//
-//        $ch = curl_init();
-//        $url = 'https://api.telegram.org/bot' . $this->token . '/sendMessage';
-//        curl_setopt($ch, CURLOPT_URL, $url);
-//        curl_setopt($ch, CURLOPT_PROXY, "socks5://$proxy");
-//        curl_setopt($ch, CURLOPT_HEADER, false);
-//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//        curl_setopt($ch, CURLOPT_POST, 1);
-//        curl_setopt($ch, CURLOPT_POSTFIELDS, ($response));
-//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        https://api.telegram.org/bot845701278:AAG-eaVtv4oNOjhYOSHGaNU6DPvb-ml3P2k/getUpdates
+
 //        $result = curl_exec($ch);
         if (isset($data->message)) {
             // получаем id чата
@@ -47,7 +32,22 @@ class telegramController extends Controller
             $payload = $data->callback_query->data;
             $this->message = null;
         }
+        $proxy='64.118.88.39:19485';
+        $response = array(
+            'chat_id' =>  331906939,
+            'text' =>$peer. ' '.$payload.' '.$this->message,
+//            'reply_markup' => $this->keyboard
+        );
 
+        $ch = curl_init();
+        $url = 'https://api.telegram.org/bot' . $this->token . '/sendMessage';
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_PROXY, "socks5://$proxy");
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, ($response));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $stage=$this->dialogTest($peer, $data);
         if ($stage!=0){
             serviceController::stageProcess($stage, $data, 2);
