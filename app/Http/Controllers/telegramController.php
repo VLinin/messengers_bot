2c082ee310bcb23aa573907c5ef17503c620e79c
Виктор <v.yu.linin@4their.ru>
2019-05-29 00:29:42 +0300

    отладка телеграмм

--- app/Http/Controllers/telegramController.php
+++ app/Http/Controllers/telegramController.php
@@ -18,23 +18,23 @@ class telegramController extends Controller
         $set_webhook='https://api.telegram.org/bot845701278:AAG-eaVtv4oNOjhYOSHGaNU6DPvb-ml3P2k/setwebhook?url=https://xn--h1aahjb.xn--p1acf/tlgrm';
         $data = json_decode($request->getContent());
 //        https://api.telegram.org/bot845701278:AAG-eaVtv4oNOjhYOSHGaNU6DPvb-ml3P2k/getUpdates
-        $proxy='64.118.88.39:19485';
-        $response = array(
-            'chat_id' =>  331906939,
-            'text' =>$data->message->chat->id. ' '.$data->message->text,
-//            'reply_markup' => $this->keyboard
-        );
-
-        $ch = curl_init();
-        $url = 'https://api.telegram.org/bot' . $this->token . '/sendMessage';
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_PROXY, "socks5://$proxy");
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, ($response));
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $result = curl_exec($ch);
+//        $proxy='64.118.88.39:19485';
+//        $response = array(
+//            'chat_id' =>  331906939,
+//            'text' =>$data->message->chat->id. ' '.$data->message->text,
+////            'reply_markup' => $this->keyboard
+//        );
+//
+//        $ch = curl_init();
+//        $url = 'https://api.telegram.org/bot' . $this->token . '/sendMessage';
+//        curl_setopt($ch, CURLOPT_URL, $url);
+//        curl_setopt($ch, CURLOPT_PROXY, "socks5://$proxy");
+//        curl_setopt($ch, CURLOPT_HEADER, false);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//        curl_setopt($ch, CURLOPT_POST, 1);
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, ($response));
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//        $result = curl_exec($ch);
         if (isset($data->message)) {
             // получаем id чата
             $peer = $data->message->chat->id;
@@ -91,7 +91,8 @@ class telegramController extends Controller
                 $dialog->save();
                 $text="Для взаимодействия с системой укажите свой мобильный телефон начиная с 8.... 
                         Это позволит связать ваши аккаунты из различных сервисов и осуществлять заказы!";
-                sendToTlgrmJob::dispatch($peer, $text, null, $this->makeKeyboardTlgrm(1,$peer,2),['next_stage'=>1,'pre_stage'=>null,'spec_info'=>null]);
+                $this->sendMsg($peer, $text, null, null,['next_stage'=>1,'pre_stage'=>null,'spec_info'=>null]);
+//                sendToTlgrmJob::dispatch($peer, $text, null, $this->makeKeyboardTlgrm(1,$peer,2),['next_stage'=>1,'pre_stage'=>null,'spec_info'=>null]);
                 return 0;
             }
         }else{
@@ -118,7 +119,8 @@ class telegramController extends Controller
                 }else{
                     $text="Для взаимодействия с системой укажите свой мобильный телефон начиная с 8.... 
                         Это позволит связать ваши аккаунты из различных сервисов и осуществлять заказы!";
-                    sendToTlgrmJob::dispatch($peer, $text, null, $this->makeKeyboardTlgrm(1,$peer,2),['next_stage'=>1,'pre_stage'=>null,'spec_info'=>null]);
+                    $this->sendMsg($peer, $text, null, null,['next_stage'=>1,'pre_stage'=>null,'spec_info'=>null]);
+//                    sendToTlgrmJob::dispatch($peer, $text, null, $this->makeKeyboardTlgrm(1,$peer,2),['next_stage'=>1,'pre_stage'=>null,'spec_info'=>null]);
                     return 0;
                 }
             }
@@ -163,5 +165,57 @@ class telegramController extends Controller
         return $kbrd;
     }
 
+    function sendMsg($from_id, $text,$photo, $keyboard, $dialoginfo){
+        $proxy='64.118.88.39:19485';
+        if($keyboard!=null){
+            $response = array(
+                'chat_id' =>  $from_id,
+                'text' => $text.'test',
+                'reply_markup' => $keyboard
+            );
+        }else{
+            $response = array(
+                'chat_id' =>  $from_id,
+                'text' => $text.'test'
+            );
+        }
+
+        $ch = curl_init();
+        $url = 'https://api.telegram.org/bot' . $this->token . '/sendMessage';
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_PROXY, "socks5://$proxy");
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, ($response));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+
+
+        if(json_decode($result)->ok){
+            if($photo!=null){
+                $response = array(
+                    'chat_id' => $from_id,
+                    'photo' => curl_file_create(__DIR__ . '/image.png')
+                );
+
+                $ch = curl_init();
+                $url = 'https://api.telegram.org/bot' . $this->token . '/sendMessage';
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_PROXY, "socks5://$proxy");
+                curl_setopt($ch, CURLOPT_HEADER, false);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, ($response));
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                $result = curl_exec($ch);
+
+                Dialog::where('chat_id','=',$from_id)->where('service_id','=',3)->update(['dialog_stage_id' => $dialoginfo['next_stage'], 'pre_stage' => $dialoginfo['pre_stage'],'spec_info' => $dialoginfo['spec_info']]);
+
+            }else{
+                Dialog::where('chat_id','=',$from_id)->where('service_id','=',3)->update(['dialog_stage_id' => $dialoginfo['next_stage'], 'pre_stage' => $dialoginfo['pre_stage'],'spec_info' => $dialoginfo['spec_info']]);
+            }
+        }
+    }
 
 }
